190e57f1c0a8b080357f9b62b9059e70985be8a8
Ansh Kevadiya <anshkevadiya7179@gmail.com>
2025-08-05 12:57:43 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from "express"
+const app = express()
+
+app.get("/api/auth/signup", (req, res) => {
+    res.send("Signup Route!");
+});
+app.get("/api/auth/login", (req, res) => {
+    res.send("Login Route!");
+});
+app.get("/api/auth/logout", (req, res) => {
+    res.send("Logout Route!");
+});
+
+app.listen(5001, () => {
+    console.log("Server is running on port 5001");
+})
