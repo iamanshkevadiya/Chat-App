1a88fda6b5247288ee6aaa521579b965ec6194f3
Harsh_Diyora <146904128+Harsh90904@users.noreply.github.com>
2025-08-07 12:31:31 +0530

    Add files via upload

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,13 @@
-import express from "express"
-import dotenv from "dotenv";
-dotenv.config();
-
-import authRoutes from "./routes/auth.route.js";
-
+import express from "express";
+import "dotenv/config";
+import authRoutes from "./routers/auth.route.js"
+import { connectDB } from "./lib/db.js";
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT  = process.env.PORT;
+
 
 app.use("/api/auth", authRoutes)
-
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+    connectDB()
 })
