14a2ad721603eba9d7578b6d6d4e00850f2a69b3
Harsh_Diyora <146904128+Harsh90904@users.noreply.github.com>
2025-08-07 12:30:28 +0530

    Add files via upload

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,9 @@
+export function signup(req,res){
+    res.send("signup router")
+}
+export function login(req,res){
+    res.send("login router")
+}
+export function logout(req,res){
+    res.send("logout router")
+}
