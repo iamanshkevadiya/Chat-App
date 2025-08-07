90948200f1b87abf811ca5e306ad7de06e5ee4df
Harsh_Diyora <146904128+Harsh90904@users.noreply.github.com>
2025-08-07 12:30:49 +0530

    Delete src directory

--- src/controllers/auth.controller.js
+++
@@ -1,9 +0,0 @@
-export function signup(req,res){
-    res.send("signup router")
-}
-export function login(req,res){
-    res.send("login router")
-}
-export function logout(req,res){
-    res.send("logout router")
-}
