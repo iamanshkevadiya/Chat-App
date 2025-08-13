3ea8e10b2728b48f597f00b60193170d01e12439
Ansh Kevadiya <anshkevadiya7179@gmail.com>
2025-08-13 13:02:02 +0530

    first commit

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,9 +1,10 @@
-export function signup(req,res){
-    res.send("signup router")
+export async function signup(req, res) {
+    res.send('Signup Route');
 }
-export function login(req,res){
-    res.send("login router")
+export async function login(req, res) {
+    res.send('Login Route');
 }
-export function logout(req,res){
-    res.send("logout router")
+
+export async function logout(req, res) {
+    res.send('Logout Route');
 }
--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,12 +1,11 @@
-import mongoose from "mongoose";
-
+import mongoose from 'mongoose';
 
 export const connectDB = async () => {
     try {
-       const conn =  await mongoose.connect(process.env.DB_URL);
+        const conn = await mongoose.connect(process.env.DB_URL);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
-        console.log("Error in connecting to MongoDB", error);
-        process.exit(1);
+        console.error("Error connecting to MongoDB:", error);
+        process.exit(1); // Exit the process with failure
     }
 }
