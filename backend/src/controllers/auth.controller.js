d6fc568f9327750557cdd087712ae28f92696352
Harsh90904 <harshdiyora6@gmail.com>
2025-08-14 12:41:52 +0530

    new commit

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,42 @@
+import User from '../models/User.js';
+import jwt from 'jsonwebtoken';
 export async function signup(req, res) {
-    res.send('Signup Route');
+    const {email,password,fullname} = req.body;
+    try {
+        if(!email || !password || !fullname) {
+            return res.status(400).json({message: "All fields are required"});
+        }
+
+        if(password.length < 6) {
+            return res.status(400).json({message:"Password must be at least 6 characters"});
+        }
+
+        const emailRegex = /^[^/s@]+@[^/s@]+\.[^/s@]+$/;
+        if(!emailRegex.test(email)) {
+            return res.status(400).json({message: "Invalid email format"});
+        }
+        const existingUser = await User.findOne({ email });
+        if(existingUser) {
+            return res.status(400).json({message: "email already exists,please use a different email"});
+        }
+
+        const idx = Math.floor(Math.random() * 100 )+ 1;
+        const rendomAvter = `https://avatar.iran.liara.run/public/${idx}.png`;
+
+        const newUser = new User({
+            email,
+            fullname,
+            password,
+            profilePic: rendomAvter
+        });
+        const token = jwt.sign({
+            userid: newUser._id,
+        },process.env.JWT_SECRET_KEY,{
+            expiresIn: '30d'
+        })
+    } catch (error) {
+        
+    }
 }
 export async function login(req, res) {
     res.send('Login Route');
--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -55,7 +55,8 @@ const User = mongoose.model('User', userSchema);
 
 
 // pre hook
-userSchema.pre('save', async function (next) {
+userSchema.pre('save', async function(next) {
+    if(!this.isModified('password')) return next();
     try {
         const salt = await bcrypt.genSalt(10);
         this.password = await bcrypt.hash(this.password, salt);
