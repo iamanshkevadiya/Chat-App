347366d04c07472e29bc87d995bf6298f4056708
Harsh90904 <harshdiyora6@gmail.com>
2025-08-19 12:59:34 +0530

    new commit

---
+++ backend/src/lib/stream.js
@@ -0,0 +1,13 @@
+import {StreamChat} from "stream-chat";
+import "dotenv/config";
+
+const apiKey = process.env.STREAM_API_KEY;
+const apiSecret = process.env.STREAM_API_SECRET;
+
+if (!apiKey || !apiSecret) {
+    console.error("stream api key or secret is missing");
+}
+
+const streamClient = StreamChat.getInstance(apiKey, apiSecret);
+
+
